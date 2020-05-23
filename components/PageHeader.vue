1f4b701616cc724c1dd2b933dea7a4a11dd36e04
Sajjad Shirazy <shirazy.sajjad@gmail.com>
2020-05-23 17:05:04 +0430

    feat: content

---
+++ components/PageHeader.vue
@@ -0,0 +1,102 @@
+<template>
+    <header ref="header" class="header-page">
+        <div class="header__content container">
+            <div ref="flex" class="flex-header">
+                <h1 ref="title" id="header-title">{{title}}</h1>
+                <div class="right-content">{{right}}</div>
+            </div>
+        </div>
+        <svg
+            class="header-effect"
+            xmlns="http://www.w3.org/2000/svg"
+            width="160"
+            height="160"
+            viewBox="0 0 160 160"
+        >
+            <path
+                id="Trazado_200"
+                data-name="Trazado 200"
+                d="M0-10,150,0l10,150S137.643,80.734,100.143,43.234,0-10,0-10Z"
+                transform="translate(0 10)"
+                fill="#fff"
+            />
+        </svg>
+    </header>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+export default Vue.extend({
+    props: ['title', 'right'],
+});
+</script>
+<style lang="scss" scoped>
+.header-page {
+    height: auto;
+    border-radius: 0 0 0 30px;
+    transition: background 0.25s ease, border-radius 0.25s ease;
+    top: 0;
+    width: 100%;
+    position: absolute;
+    background: var(--vs-theme-bg2);
+    .header__content {
+        padding: 2rem 2.5rem;
+        position: relative;
+        display: flex;
+        flex-direction: column;
+        justify-content: flex-end;
+        height: 200px;
+        padding-top: 0;
+        padding-bottom: 0;
+        background: transparent;
+        padding: 0 40px !important;
+        .flex-header {
+            width: 100%;
+            display: flex;
+            align-items: flex-end;
+            justify-content: space-between;
+            margin-bottom: 70px;
+            h1 {
+                // font-size: 35px;
+                // margin: 0;
+                // padding: 13px 0 15px;
+                // font-weight: 600;
+            }
+            .right-content {
+                display: flex;
+                align-items: center;
+                justify-content: center;
+                list-style: none;
+                padding-bottom: 13px;
+                margin: 0;
+            }
+        }
+        &:after {
+            content: '';
+            position: absolute;
+            bottom: 0;
+            left: 50%;
+            transform: translate(-50%);
+            width: 110%;
+            height: 100%;
+            z-index: -1;
+            box-shadow: 0 25px 20px -25px rgba(0, 0, 0, 0.1);
+        }
+    }
+    .header-effect {
+        position: absolute;
+        bottom: 0;
+        transform: rotate(0deg);
+        max-width: 40px;
+        max-height: 40px;
+        z-index: 200;
+        right: 0;
+        bottom: -38px;
+        transition: all 0.25s ease;
+        #Trazado_200 {
+            fill: var(--vs-theme-bg2) !important;
+            stroke: var(--vs-theme-bg2) !important;
+        }
+    }
+}
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <Navbar />
-        <div class="content">
+        <div>
             <Sidebar />
             <nuxt />
         </div>
@@ -21,46 +21,4 @@ export default Vue.extend({
 });
 </script>
 
-<style src="~/assets/styles/variables.scss" lang="scss"></style>
-<style lang="scss">
-@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@100;400;500;600;700&display=swap');
-
-* {
-    font-family: Poppins, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans,
-        Droid Sans, Helvetica Neue, sans-serif;
-    word-spacing: 1px;
-    -ms-text-size-adjust: 100%;
-    -webkit-text-size-adjust: 100%;
-    -moz-osx-font-smoothing: grayscale;
-    -webkit-font-smoothing: antialiased;
-    box-sizing: border-box;
-    font-weight: 100;
-    margin: 0;
-}
-
-html {
-    background: var(--vs-theme-bg2);
-}
-
-p {
-    font-size: 0.85rem;
-}
-
-h1 {
-    font-size: 35px;
-    font-weight: 600;
-}
-
-.content {
-    background-color: var(--vs-theme-bg) !important;
-    margin-left: 260px;
-    padding-left: 0 !important;
-    display: block;
-    margin-top: 57px;
-    position: relative;
-    margin-bottom: 0;
-    padding-top: 200px !important;
-    transition: all 0.25s ease;
-    width: calc(100% - 260px);
-}
-</style>
+<style src="~/assets/styles/styles.scss" lang="scss"></style>
